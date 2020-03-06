31ae3f715b16c283c258599108bebaf3dce2b095
Pete <44038179+PShipstone@users.noreply.github.com>
2020-03-06 23:00:43 +0000

    Added properties

--- BHFB.Entities/User.cs
+++ BHFB.Entities/User.cs
@@ -2,7 +2,13 @@
 
 namespace BHFB.Entities
 {
-    public class Class1
+    public class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int CreatedById { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public int UpdatedOnId { get; set; }
     }
 }
